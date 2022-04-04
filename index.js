81f7f516b64dc34358673d17d8bb075e7a2f6032
kashi <leonc4667@gmail.com>
2022-04-04 16:18:35 -0400

    keystone practice

---
+++ index.js
@@ -0,0 +1,67 @@
+const dotenv = require('dotenv').config()
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { PasswordAuthStrategy } = require('@keystonejs/auth-password')
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'practica';
+const adapterConfig = {
+   mongoUri: process.env.MONGO_URI,
+   };
+
+
+const PostSchema = require('./list/Post')
+const UserSchema = require('./list/User')
+
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+  cookieSecret: process.env.COOKIE_SECRET,
+});
+
+const isAdmin = ({authentication: {item: user}}) =>{
+  return !!user && !!user.isAdmin
+}
+
+const isLoggedIn = ({authentication: {item: user}}) =>{
+  return !!user 
+}
+
+keystone.createList('Post', {
+  fields: PostSchema.fields,
+  access: {
+    read: true,
+    create: isLoggedIn,
+    update: isLoggedIn,
+    delete: isLoggedIn,
+  }
+})
+
+keystone.createList('User', {
+  fields: UserSchema.fields,
+  access: {
+    read: true,
+    create: isAdmin,
+    update: isAdmin,
+    delete: isAdmin,
+  }
+})
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User',
+  config: {
+    identityField: 'email',
+    secretField: 'password'
+  }
+})
+
+
+module.exports = {
+  keystone,
+  apps: [new GraphQLApp(), new AdminUIApp({ name: PROJECT_NAME,
+     enableDefaultRoute: true,
+     authStrategy,
+     //isAccessAllowed: isLoggedIn
+    })],
+};
